d3cb9c87f68e4eaa32caa220ca61c8862a3d341e
can_s_officiall <50926214+cansofficall@users.noreply.github.com>
2020-07-25 13:46:25 +0300

    Add files via upload

---
+++ CnsAdScn.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def findAdmin():
+	f = open("link.txt","r");
+	link = raw_input("Enter Site Name \n(ex : example.com or www.example.com ): ")
+	print "\n\nAvilable links : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "OK => ",req_link
+npm install figlet
+def Credit():
+	var figlet = require('figlet');
+ 
+figlet('WEBCNS', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+Credit()
+findAdmin()
